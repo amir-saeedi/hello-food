3a68f0daa45113a7407fa135cf28b79fc4b37c46
amir-saeedi <saeediamir1381@gmail.com>
2021-09-24 16:26:21 +0330

    'project'

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.rslides').responsiveSlides({
+        speed:500
+    });
+});
+$(document).ready(function(){
+    $("#owl-demo").owlCarousel({
+        autoPlay:true
+    });
+});
+$(document).ready(function(){
+    $(".inline").colorbox({
+        inline:true,
+        width:"450"
+    });
+});
+$(document).ready(function(){
+    $('#scroll').mCustomScrollbar({
+        scrollButtons:{
+            enable:"true"
+        },
+        theme:"dark-2",
+    });
+});
+$(document).ready(function(){
+    $(".sticky-menu").sticky({topSpacing:0});
+  });
+  $(document).ready(function(){
+      $('.nav-bar').click(function(){
+          $('.responsive-nav ul').toggle(200);
+      });
+  });
